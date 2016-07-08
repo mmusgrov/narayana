55770d14c2e79b49ab74674b64dbe8bd6c26e29a
Michael Musgrove <mmusgrov@redhat.com>
2016-07-08 12:09:01 +0100

    JBTM-2685 exclude more tests

--- scripts/hudson/narayana.sh
+++ scripts/hudson/narayana.sh
@@ -214,7 +214,7 @@ function build_narayana {
   if [ $JAVA_VERSION="9-ea" ]; then
     ORBARG="-Djacorb-disabled -Didlj-disabled -Dopenjdk-disabled"
     #XPROF=",arq" # j9 TODO arquillian based tests fail unless the arq profile is active
-    XPROF=community
+    XPROF=community,arq
   elif [ $IBM_ORB = 1 ]; then
     ORBARG="-Dibmorb-enabled -Djacorb-disabled -Didlj-disabled -Dopenjdk-disabled"
     ${JAVA_HOME}/bin/java -version 2>&1 | grep IBM
