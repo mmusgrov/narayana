51044ca55840ad31ae33f6ed34534ae8be07f7fd
Michael Musgrove <mmusgrov@redhat.com>
2026-05-18 17:15:54 +0100

    JBTM-4038

--- ArjunaCore/arjuna/tests/classes/com/hp/mwtests/ts/arjuna/objectstore/jgroups/JGroupsSlotsClusterTest.java
+++ ArjunaCore/arjuna/tests/classes/com/hp/mwtests/ts/arjuna/objectstore/jgroups/JGroupsSlotsClusterTest.java
@@ -6,11 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 import java.io.Serializable;
-import java.lang.reflect.Method;
-import java.net.URL;
-import java.net.URLClassLoader;
 import java.util.Arrays;
 
 /**
@@ -24,8 +20,8 @@ public class JGroupsSlotsClusterTest {
     private static final String CLUSTER_NAME = "classloader-test-cluster";
     private static final long TIMEOUT = 10000L;
 
-    private IsolatedCache cache1;
-    private IsolatedCache cache2;
+    private JGroupsIsolatedCache cache1;
+    private JGroupsIsolatedCache cache2;
 
     /**
      * Wrapper for byte[] that implements proper hashCode/equals and Serializable.
@@ -78,110 +74,13 @@ public class JGroupsSlotsClusterTest {
         }
     }
 
-    /**
-     * Helper class that wraps a ReplCache instance loaded in an isolated class loader
-     */
-    static class IsolatedCache {
-        private final ClassLoader classLoader;
-        private final Object cacheInstance;
-        private final Class<?> cacheClass;
-
-        IsolatedCache(String props, String clusterName) throws Exception {
-            // Create isolated class loader
-            this.classLoader = createIsolatedClassLoader();
-
-            // Load ReplCache class in the isolated class loader
-            this.cacheClass = classLoader.loadClass("org.jgroups.blocks.ReplCache");
-
-            // Create instance: new ReplCache(props, clusterName)
-            this.cacheInstance = cacheClass
-                .getConstructor(String.class, String.class)
-                .newInstance(props, clusterName);
-        }
-
-        private static URLClassLoader createIsolatedClassLoader() throws Exception {
-            // Get current classpath
-            String classpath = System.getProperty("java.class.path");
-            String[] paths = classpath.split(File.pathSeparator);
-
-            URL[] urls = new URL[paths.length];
-            for (int i = 0; i < paths.length; i++) {
-                File file = new File(paths[i]);
-                urls[i] = file.toURI().toURL();
-            }
-
-            // Create new class loader with null parent to ensure isolation
-            // (using system class loader as parent to load JDK classes)
-            return new URLClassLoader(urls, ClassLoader.getSystemClassLoader().getParent());
-        }
-
-        void start() throws Exception {
-            Method startMethod = cacheClass.getMethod("start");
-            startMethod.invoke(cacheInstance);
-        }
-
-        void stop() throws Exception {
-            Method stopMethod = cacheClass.getMethod("stop");
-            stopMethod.invoke(cacheInstance);
-        }
-
-        void setCallTimeout(long timeout) throws Exception {
-            Method method = cacheClass.getMethod("setCallTimeout", long.class);
-            method.invoke(cacheInstance, timeout);
-        }
-
-        void setCachingTime(long cachingTime) throws Exception {
-            Method method = cacheClass.getMethod("setCachingTime", long.class);
-            method.invoke(cacheInstance, cachingTime);
-        }
-
-        void setDefaultReplicationCount(short count) throws Exception {
-            Method method = cacheClass.getMethod("setDefaultReplicationCount", short.class);
-            method.invoke(cacheInstance, count);
-        }
-
-        void put(Object key, Object value, short replCount, long timeout) throws Exception {
-            Method putMethod = cacheClass.getMethod("put", Object.class, Object.class, short.class, long.class);
-            putMethod.invoke(cacheInstance, key, value, replCount, timeout);
-        }
-
-        Object get(Object key) throws Exception {
-            Method getMethod = cacheClass.getMethod("get", Object.class);
-            return getMethod.invoke(cacheInstance, key);
-        }
-
-        void remove(Object key) throws Exception {
-            Method removeMethod = cacheClass.getMethod("remove", Object.class);
-            removeMethod.invoke(cacheInstance, key);
-        }
-
-        int getClusterSize() throws Exception {
-            Method method = cacheClass.getMethod("getClusterSize");
-            return (Integer) method.invoke(cacheInstance);
-        }
-
-        String getLocalAddressAsString() throws Exception {
-            Method method = cacheClass.getMethod("getLocalAddressAsString");
-            return (String) method.invoke(cacheInstance);
-        }
-
-        String dump() throws Exception {
-            Method method = cacheClass.getMethod("dump");
-            return (String) method.invoke(cacheInstance);
-        }
-
-        ClassLoader getClassLoader() {
-            return classLoader;
-        }
-    }
-
     @BeforeEach
     public void setUp() throws Exception {
         System.out.println("\n=== Setting up isolated class loader test ===");
 
         // Create two cache instances in separate class loaders
-        cache1 = new IsolatedCache(PROPS, CLUSTER_NAME);
-        cache2 = new IsolatedCache(PROPS, CLUSTER_NAME);
+        cache1 = new JGroupsIsolatedCache(PROPS, CLUSTER_NAME);
+        cache2 = new JGroupsIsolatedCache(PROPS, CLUSTER_NAME);
 
         // Verify they have different class loaders
         Assertions.assertNotSame(cache1.getClassLoader(), cache2.getClassLoader(),
